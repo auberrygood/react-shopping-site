f922279680b1577c795b401ebbd68373799c9e4f
Aubrey Sanders <aubrey.sandrs@gmail.com>
2021-05-12 04:12:15 +0000

    completed Connect addMelonToCart with MelonCard section of lab

---
+++ static/js/shoppingSite.jsx
@@ -0,0 +1,61 @@
+function App() {
+  const [melons, setMelons] = React.useState({});
+  const [shoppingCart, setShoppingCart] = React.useState({});
+  
+function addMelonToCart(melonCode) {
+  setShoppingCart((currentShoppingCart) => {
+    const newShoppingCart = Object.assign({}, currentShoppingCart);
+
+    if (newShoppingCart[melonCode]) {
+      newShoppingCart[melonCode] += 1;
+    } else {
+      newShoppingCart[melonCode] = 1;
+    }
+
+    console.log(shoppingCart);
+    return newShoppingCart;
+  })
+}
+
+
+
+  React.useEffect(() => {
+    fetch("/api/melons")
+      .then((response) => response.json())
+      .then((melonData) => setMelons(melonData));
+  }, []);
+  return (
+    <ReactRouterDOM.BrowserRouter>
+      <Navbar logo="/static/img/watermelon.png" brand="Ubermelon">
+        <ReactRouterDOM.NavLink
+          to="/shop"
+          activeClassName="navlink-active"
+          className="nav-link"
+        >
+          Shop for Melons
+        </ReactRouterDOM.NavLink>
+        <ReactRouterDOM.NavLink
+          to="/cart"
+          activeClassName="navlink-active"
+          className="nav-link"
+        >
+          Shopping Cart
+        </ReactRouterDOM.NavLink>
+      </Navbar>
+
+      <div className="container-fluid">
+        <ReactRouterDOM.Route exact path="/">
+          <Homepage />
+        </ReactRouterDOM.Route>
+        <ReactRouterDOM.Route exact path="/shop">
+          <AllMelonsPage melons={melons} addMelonToCart={addMelonToCart}/>
+        </ReactRouterDOM.Route>
+        <ReactRouterDOM.Route exact path="/cart">
+          <ShoppingCartPage />
+        </ReactRouterDOM.Route>
+      </div>
+    </ReactRouterDOM.BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
